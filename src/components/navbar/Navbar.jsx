9b8fd3d30445bb5a80db8c1893c4ba02f4bb6275
shikhapandey <128817425+pandeyshikh@users.noreply.github.com>
2024-09-02 08:06:21 +0530

    InstaVibe-Social APP

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,7 +20,7 @@ const Navbar = () => {
     <div className="navbar">
       <div className="left">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span>Shikha Social</span>
+          <span>InstaVibe</span>
         </Link>
         <HomeOutlinedIcon />
         {darkMode ? (
